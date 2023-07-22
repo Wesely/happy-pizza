dcadb568c11afbf62deeb76643fa3890ccd9f2d9
wesely <wesely.ong@gmail.com>
2023-07-22 17:26:46 +0800

    [DEV-2] Setup Hilt for Retrofit services injection
    - compileOptions now using JavaVersion.VERSION_17
    - add Hilt dependencies
    - we can inject AirQualityApiService now

--- app/src/main/java/tw/wesley/uiassignment/AirQualityApplication.kt
+++ app/src/main/java/tw/wesley/uiassignment/AirQualityApplication.kt
@@ -1,8 +1,10 @@
 package tw.wesley.uiassignment
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class AirQualityApplication : Application() {
     override fun onCreate() {
         super.onCreate()
--- app/src/main/java/tw/wesley/uiassignment/viewmodel/AirQualityViewModel.kt
+++ app/src/main/java/tw/wesley/uiassignment/viewmodel/AirQualityViewModel.kt
@@ -2,22 +2,22 @@ package tw.wesley.uiassignment.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
-import retrofit2.Retrofit
 import timber.log.Timber
 import tw.wesley.uiassignment.network.AirQualityApiService
+import javax.inject.Inject
 
-class AirQualityViewModel : ViewModel() {
+@HiltViewModel
+class AirQualityViewModel @Inject constructor(
+    private val airQualityApiService: AirQualityApiService
+) : ViewModel() {
+
 
     // TODO: This is just a POC, remove it later in task #3
     fun printAirQualityInLog() {
-        val apiService = Retrofit.Builder()
-            .baseUrl("https://data.epa.gov.tw")
-            .build()
-            .create(AirQualityApiService::class.java)
-
         viewModelScope.launch {
-            Timber.d(apiService.getAirQualityData().string())
+            Timber.d(airQualityApiService.getAirQualityData().string())
         }
     }
 }
