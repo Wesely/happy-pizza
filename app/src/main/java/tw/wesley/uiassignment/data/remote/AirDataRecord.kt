6402f5939a8fd73a67229547bda631e0f8a7d717
wesely <wesely.ong@gmail.com>
2023-07-23 15:58:37 +0800

    Data preserved in database

--- app/src/main/java/tw/wesley/uiassignment/data/remote/AirDataRecord.kt
+++ app/src/main/java/tw/wesley/uiassignment/data/remote/AirDataRecord.kt
@@ -5,7 +5,7 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class AirDataRecord(
-    @Json(name = "siteid") val siteId: String?,
+    @Json(name = "siteid") val siteId: String,
     @Json(name = "sitename") val siteName: String,
     @Json(name = "county") val county: String,
     @Json(name = "pm2.5") val pm25: String,
